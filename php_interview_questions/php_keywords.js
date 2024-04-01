14dd52c5c82105102aaa77c63379bba07d71af7b
RaihanaFerdousi <raihanaferdousi110@gmail.com>
2024-04-01 14:17:00 +0600

    updated

---
+++ php_interview_questions/php_keywords.js
@@ -0,0 +1,53 @@
+// variable
+// general php knoledge
+// html    
+// local development environment
+// object oriented php
+// php abstract classes
+// php advanced
+// php and html
+// php and javascript
+// php anonymous function
+// php autoloading
+// php benefits
+// php constants
+// php cookies
+// php data types
+// php data validation and sanitization
+// php database
+// php design patterns
+// php encryption alogrithm
+// php excel
+// php exceptions
+// php features
+// php file handling
+// php form validation
+// php ftp
+// php function
+// php history
+// php ibxml
+// php image handling
+// php installation
+// php interfaces
+// php mail
+// php middleware
+// php miscellaneous
+// php mysql
+// php namespaces
+// php networking
+// php oop
+// php operators
+// php output control
+// php pagination
+// php password hashing
+// php performance
+// php regular expressions
+// php security
+// php sessions and cookies
+// php sessions
+// php string manipulation
+// php superglobals
+// php syntax
+// php troubleshooting
+// php web security
+// web development with php
