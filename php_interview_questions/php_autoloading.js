9a5ed11f8b2204f5db9da708d4a306496b5aaa67
RaihanaFerdousi <raihanaferdousi110@gmail.com>
2024-04-03 01:54:45 +0600

    updated

--- php_interview_questions/php_autoloading.js
+++ php_interview_questions/php_autoloading.js
@@ -1,5 +1,4 @@
-// Question 1:
-// What is the concept of autoloading in PHP? How does it work and how can you implement it in your code?
+// Question 1: What is the concept of autoloading in PHP? How does it work and how can you implement it in your code?
 
 // Explanation
 // Autoloading in PHP eliminates the need to manually include all the required class files in your code. It dynamically loads the class files on-demand, improving code organization and reducing manual effort. Autoloading can be implemented by registering an autoloader function using the spl_autoload_register() function. This function allows you to define your own autoloader logic, which is triggered whenever a class is accessed but not yet defined. For more information, you can refer to the PHP documentation: http://php.net/manual/en/language.oop5.autoload.php
--- php_interview_questions/php_keywords.js
+++ php_interview_questions/php_keywords.js
@@ -2,49 +2,49 @@
 // general php knowledge ✅
 // html ✅
 // local development environment ✅
-// object oriented php
-// php abstract classes
+// object oriented php ✅
+// php abstract classes ✅
 // php advanced
 // php and html ✅
 // php and javascript ✅
-// php anonymous function
-// php autoloading
+// php anonymous function ✅
+// php autoloading ✅
 // php benefits ✅
-// php constants
-// php cookies
+// php constants ✅
+// php cookies ✅
 // php data types ✅
-// php data validation and sanitization
+// php data validation and sanitization ✅
 // php database
 // php design patterns
 // php encryption alogrithm
 // php excel
-// php exceptions
+// php exceptions ✅
 // php features ✅
 // php file handling ✅
-// php form validation
+// php form validation ✅
 // php ftp
 // php function ✅
 // php history ✅
 // php ibxml
-// php image handling
+// php image handling ✅
 // php installation ✅
-// php interfaces
-// php mail
+// php interfaces ✅
+// php mail ✅
 // php middleware
 // php miscellaneous
 // php mysql
-// php namespaces
+// php namespaces ✅
 // php networking
-// php oop
-// php operators
+// php oop ✅
+// php operators ✅
 // php output control
-// php pagination
+// php pagination ✅
 // php password hashing
 // php performance
 // php regular expressions
 // php security
-// php sessions and cookies
-// php sessions
+// php sessions and cookies ✅
+// php sessions ✅
 // php string manipulation ✅
 // php superglobals ✅
 // php syntax ✅
